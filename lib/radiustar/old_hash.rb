cedf7e17dc09ddee10d62be233672ca38cd3aaea
Siwei Shen <shensiwei@youku.com>
2012-12-14 16:37:32 +0800

    added commets for using the dictionary folder, and do not make monkey patch for Hash if using ruby 1.9

--- lib/radiustar/old_hash.rb
+++ lib/radiustar/old_hash.rb
@@ -1,12 +1,14 @@
-#
-# Monkey-patch the default ruby version < 1.9.0 Hash class to provide
-# the same interface to the radiustar libs.
-#
-class Hash
-  # Implementation of the ruby-1.9.x key function:
-  # http://www.ruby-doc.org/core-1.9.3/Hash.html#method-i-key
-  def key(value)
-    self.index value
+# only patch for ruby 1.8
+if (RUBY_VERSION =~ /^1\.8/) == 0
+  #
+  # Monkey-patch the default ruby version < 1.9.0 Hash class to provide
+  # the same interface to the radiustar libs.
+  #
+  class Hash
+    # Implementation of the ruby-1.9.x key function:
+    # http://www.ruby-doc.org/core-1.9.3/Hash.html#method-i-key
+    def key(value)
+      self.index value
+    end
   end
 end
-
